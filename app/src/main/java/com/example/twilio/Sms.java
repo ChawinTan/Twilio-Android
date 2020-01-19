e24a24f265bf563d92766b2c56b739bd33205fe9
chawin <chawin15@gmail.com>
2020-01-19 10:03:14 +0800

    Sms feature done

--- app/src/main/java/com/example/twilio/Sms.java
+++ app/src/main/java/com/example/twilio/Sms.java
@@ -7,9 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sms extends AppCompatActivity {
 
     EditText editText;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +33,12 @@ public class Sms extends AppCompatActivity {
 
         Button smsButton = findViewById(R.id.button_sms);
 
+        mQueue = Volley.newRequestQueue(this);
+
         smsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println("================== Click ========================");
                 sendSms();
             }
         });
@@ -30,6 +48,32 @@ public class Sms extends AppCompatActivity {
         String url = "https://rackley-octopus-5384.twil.io/outbound-sms";
         editText = (EditText) findViewById(R.id.sms_phone);
         String phoneNum = editText.getText().toString();
+
         System.out.println(phoneNum);
+
+        Map<String, String> params = new HashMap();
+        params.put("number", phoneNum);
+
+        JSONObject paramenters = new JSONObject(params);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, paramenters,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String success = response.getString("success");
+                            System.out.println(success);
+                        } catch(JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+        });
+
+        mQueue.add(request);
     }
 }
