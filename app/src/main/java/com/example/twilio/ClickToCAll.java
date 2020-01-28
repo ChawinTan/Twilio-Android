6160f704fb7b33b90832dcc8548617bc17eda560
chawin <chawin15@gmail.com>
2020-01-28 11:45:01 +0800

    set up click to call

--- app/src/main/java/com/example/twilio/ClickToCAll.java
+++ app/src/main/java/com/example/twilio/ClickToCAll.java
@@ -3,12 +3,35 @@ package com.example.twilio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class ClickToCAll extends AppCompatActivity {
+
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_to_call);
+
+        Button callButton = findViewById(R.id.button_call);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("================== Click ========================");
+                callTwiml();
+            }
+        });
+    }
+
+    public void callTwiml() {
+        System.out.println("================== Click to call ========================");
     }
 }
