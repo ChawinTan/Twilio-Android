42c90422e1b5b47cca097cf30190b0e176bf1b78
chawin <chawin15@gmail.com>
2020-01-31 14:01:17 +0800

    add click to call functionality

--- app/src/main/java/com/example/twilio/ClickToCAll.java
+++ app/src/main/java/com/example/twilio/ClickToCAll.java
@@ -6,8 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ClickToCAll extends AppCompatActivity {
 
@@ -33,5 +40,26 @@ public class ClickToCAll extends AppCompatActivity {
 
     public void callTwiml() {
         System.out.println("================== Click to call ========================");
+        String url = "https://rackley-octopus-5384.twil.io/android-call";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String success = response.getString("success");
+                    System.out.println("================== Call success ========================");
+                    System.out.println(success);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
     }
+
 }
